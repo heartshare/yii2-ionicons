5930f2d95c36b6f5a02a8a57ef7fe9b0f11a06b6
Sergey Kasatkin <spam@onsky.ru>
2015-04-02 20:41:18 +0300

    just simple bundle for the start

---
+++ AssetBundle.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * AssetBundle.php
+ * @author Sergey Kasatkin spam@onsky.ru
+ */
+
+namespace gietos\yii\ionicons;
+
+/**
+ * Class AssetBundle
+ * @package gietos\yii\ionicons
+ */
+class AssetBundle extends \yii\web\AssetBundle
+{
+    /**
+     * @inherit
+     */
+    public $sourcePath = '@vendor/driftyco/ion-icons';
+
+    /**
+     * @inherit
+     */
+    public $css = [
+        'css/ionicons.min.css',
+    ];
+
+    /**
+     * Initializes the bundle.
+     * Set publish options to copy only necessary files (in this case css and font folders)
+     */
+    public function init()
+    {
+        parent::init();
+
+        $this->publishOptions['beforeCopy'] = function ($from, $to) {
+            return preg_match('%/(fonts|css)%', $from);
+        };
+    }
+}
